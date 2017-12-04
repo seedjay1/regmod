640a87500bbfa0e55a47b0e9b230cc6e09d29b0b
sherifffruitfly <sherifffruitfly@gmail.com>
2017-12-03 22:56:26 -0800

    jhg

--- Cars_Script.R
+++ Cars_Script.R
@@ -1,2 +1,5 @@
 # script for cars data analysis code
 
+library(ggplot2) #for plots
+data(mtcars)
+head(mtcars)
---
+++ scratchwork.R
@@ -0,0 +1,80 @@
+# create sample from unit disk
+set.seed(42) # for repro
+numpoints <- 100
+radius <- 1
+
+points.disk_sample <- NULL
+for (i in 1:numpoints)
+{
+  r <- radius * sqrt(runif(1)) # because area is proportional to r^2
+  degs <- 360*runif(1)
+  
+  theta <- 2*pi*degs/360
+  
+  x <- r * sin(theta)
+  y <- r * cos(theta)
+  
+  points.disk_sample[[i]] <-c(x, y)
+}
+
+
+# create unit circle points
+points.circle_sample <- NULL
+for (i in 1:numpoints)
+{
+  r <- radius 
+  degs <- 360 * (i/numpoints)
+  
+  theta <- 2*pi*degs/360
+  
+  x <- r * sin(theta)
+  y <- r * cos(theta)
+  
+  points.circle_sample[[i]] <-c(x, y)
+}
+
+
+# create transformation T
+T <- matrix(c(1, 2, 3, 2), nrow=2)
+
+
+# calculate T(samples)
+points.tdisk <- NULL
+points.tcircle <- NULL
+for (i in 1:numpoints)
+{
+  points.tdisk[[i]] <- T %*% points.disk_sample[[i]]
+  points.tcircle[[i]] <- T %*% points.circle_sample[[i]]
+}
+
+
+# create plot datasets
+points.plot.circle <- NULL
+points.plot.disk <- NULL
+points.plot.tcircle <- NULL
+points.plot.tdisk <- NULL
+for (i in 1:numpoints)
+{
+  # unit circle
+  points.plot.circle$x[i] <- points.circle_sample[[i]][[1]]
+  points.plot.circle$y[i] <- points.circle_sample[[i]][[2]]
+  
+  # unit disk
+  points.plot.disk$x[i] <- points.disk_sample[[i]][[1]]
+  points.plot.disk$y[i] <- points.disk_sample[[i]][[2]]
+  
+  # T(unit circle)
+  points.plot.tcircle$x[i] <- points.tcircle[[i]][[1]]
+  points.plot.tcircle$y[i] <- points.tcircle[[i]][[2]]
+  
+  # T(unit disk)
+  points.plot.tdisk$x[i] <- points.tdisk[[i]][[1]]
+  points.plot.tdisk$y[i] <- points.tdisk[[i]][[2]]
+}
+
+plot(x=points.plot.circle$x, y=points.plot.circle$y, asp=T)
+plot(x=points.plot.disk$x, y=points.plot.disk$y, asp=T)
+
+
+plot(x=points.plot.tcircle$x, y=points.plot.tcircle$y, asp=T)
+plot(x=points.plot.tdisk$x, y=points.plot.tdisk$y, asp=T)
