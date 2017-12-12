38460330d755e8f7b106ac3aaca673cc9a8bde74
sherifffruitfly <sherifffruitfly@gmail.com>
2017-12-12 13:03:47 -0800

    progress...

--- Cars_Script.R
+++ Cars_Script.R
@@ -81,16 +81,16 @@ print(g1)
 
 
 # look at variable correlations to decide which variables to throw away
-#col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))
-#corrplot(car_corrs, method="color", col=col(200),  
-#         type="upper", order="hclust", 
-#         addCoef.col = "black", # Add coefficient of correlation
-#         tl.col="black", tl.srt=45, #Text label color and rotation
-#         # Combine with significance
-#         p.mat = p.mat, sig.level = 0.01, insig = "blank", 
-#         # hide correlation coefficient on the principal diagonal
-#         diag=FALSE 
-#)
+col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))
+corrplot(car_corrs, method="color", col=col(200),  
+         type="upper", order="hclust", 
+         addCoef.col = "black", # Add coefficient of correlation
+         tl.col="black", tl.srt=45, #Text label color and rotation
+         # Combine with significance
+         p.mat = p.mat, sig.level = 0.01, insig = "blank", 
+         # hide correlation coefficient on the principal diagonal
+         diag=FALSE 
+)
 
 #corrplot(car_corrs, method="number")
 
