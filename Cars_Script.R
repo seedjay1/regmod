43e303cee225cb34e6e9230c2972311bf36bcf9b
seedjay1 <see_d_jay@yahoo.com>
2017-12-18 09:26:46 -0800

    almost done

--- Cars_Script.R
+++ Cars_Script.R
@@ -119,7 +119,7 @@ best.subset.by.cp
 best.subset.by.bic <- which.min(best.subset.summary$bic)
 best.subset.by.bic
 
-par(mfrow=c(2,2))
+par(mfrow = c(2, 2), oma = c( 0, 0, 2, 0 ))
 plot(best.subset$rss, xlab="Number of Variables", ylab="RSS", type="l")
 plot(best.subset.summary$adjr2, xlab="Number of Variables", ylab="Adjusted RSq", type="l")
 points(best.subset.by.adjr2, best.subset.summary$adjr2[best.subset.by.adjr2], col="red", cex =2, pch =20)
@@ -127,5 +127,6 @@ plot(best.subset.summary$cp, xlab="Number of Variables", ylab="CP", type="l")
 points(best.subset.by.cp, best.subset.summary$cp[best.subset.by.cp], col="red", cex =2, pch =20)
 plot(best.subset.summary$bic, xlab="Number of Variables", ylab="BIC", type="l")
 points(best.subset.by.bic, best.subset.summary$bic[best.subset.by.bic], col="red", cex =2, pch =20)
+title( "Regression Variable Selection KPIs", outer = TRUE )
 
-coef(best.subset, 3)
+#coef(best.subset, 3)
